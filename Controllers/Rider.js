a7b224f31dfd683a282914f864ab45e560e06825
Mrudul Kolambe <mrudulkolambe02@gmail.com>
2024-01-18 17:07:29 +0530

    changes

--- Controllers/Rider.js
+++ Controllers/Rider.js
@@ -68,7 +68,7 @@ const orderAssign = async (req, res) => {
     try {
         const check = await Order.findOne({ _id: req.params._id });
         const checkRider = await Rider.findOne({ _id: req.rider._id });
-        console.log(check.status != "new" && checkRider.orders.length === 2)
+        console.log(check.status != "new" && checkRider.orders.length === 2, check.status, checkRider.orders.length)
         if (check.status != "new" && checkRider.orders.length === 2) {
             return res.json({ error: true, message: "Cannot Assign", rider: checkRider })
         } else {
