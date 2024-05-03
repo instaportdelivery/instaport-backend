58f7e0dba29c034bf0c450afdeeac33dc054657e
mrudulkolambe <mrudulkolambe02@gmail.com>
2024-05-04 01:16:50 +0530

    changes

--- Controllers/Order.js
+++ Controllers/Order.js
@@ -67,7 +67,7 @@ const orderByIDCustomerApp = async (req, res) => {
 //Update Order
 const updateOrder = async (req, res) => {
     const order = await Order.findOne({ _id: req.body._id })
-    const rider = await Rider.findById(order.rider);
+    const rider = await Rider.findById(order.rider._id);
     if (!order) {
         res.json({ error: true, message: "Something Went Wrong", order: undefined })
     }
